92c1931721823665f077e93149d2f7d209294fa3
Sof7ik <alekse-bychkov@rambler.ru>
2019-09-17 16:25:17 +0300

    Сводные данные о сессии

---
+++ cabinet.php
@@ -0,0 +1,51 @@
+<?
+require ('connection.php');
+session_start();
+
+if (!isset($_SESSION['username'])) {
+	echo '<meta http-equiv="refresh" content="0;index.php">'; //идем на главную страницу
+	exit;
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="styles/style_cart.css">
+</head>
+<body>
+
+<div class="login">
+	
+	<?
+		echo "<p class='name'>Вы вошли как <a href='cabinet.php'>" . $_SESSION['username'] . "</a></p>";
+		echo "Фамилия - " . $_SESSION['surname'];
+		echo "<br>";
+		echo "Роль - " . $_SESSION['role'];
+		echo "<br>";
+		echo "ID User - " . $_SESSION['userId'];
+		echo "<br>";
+		echo "Login - " . $_SESSION['login'];
+	?>
+
+	<div style="display: flex; flex-flow: row wrap; justify-content: center">
+
+		<?
+		if ($_SESSION['role'] == 2) {
+			echo '<a href="admin.php" class="cart">Админ</a>';//идем на страницу админа
+			
+		}
+		?>
+
+		<a href="cart.php" class="cart">ГЛАВНАЯ</a>
+		<a href="logout.php" class="cart">ВЫХОД</a>
+
+	</div>
+
+</div>
+
+</body>
+</html>
+
